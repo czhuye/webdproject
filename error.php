a9424f2498b31cc036162719a79814bb58a1be21
czhuye <65668139+czhuye@users.noreply.github.com>
2021-04-22 17:26:54 -0500

    error.php
    error.php

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>WEB DEV Project</title>
         <link rel="stylesheet" type="text/css" href="styles.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     </head>
